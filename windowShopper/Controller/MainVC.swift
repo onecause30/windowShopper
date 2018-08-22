d716b305e52f28a6f3811ec9740ac034b691da76
Anthony Hall <anthonyhall@anthonys-imac.local>
2018-08-21 22:53:51 -0700

    Initial Commit

---
+++ windowShopper/Controller/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  windowShopper
+//
+//  Created by Anthony Hall on 8/21/18.
+//  Copyright © 2018 Anthony Hall. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ windowShopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,27 @@
+//
+//  CurrencyTxtField.swift
+//  windowShopper
+//
+//  Created by Anthony Hall on 8/21/18.
+//  Copyright © 2018 Anthony Hall. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2491170805)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
+
+}
